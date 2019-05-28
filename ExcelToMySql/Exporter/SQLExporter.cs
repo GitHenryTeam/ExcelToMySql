c83786df5eb445d22c5692ef876eb213d5a71e06
wcqu <15827025800@126.com>
2019-05-28 16:39:32 +0800

    提交源码

---
+++ ExcelToMySql/Exporter/SQLExporter.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace excel2json
+{
+    class SQLExporter
+    {
+		enum RowType
+		{
+			INTEGER = 0,
+			text,
+		}
+		RowType[] _types = null;
+        DataTable m_sheet;
+        int m_headerRows;
+
+        /// <summary>
+        /// 初始化内部数据
+        /// </summary>
+        /// <param name="sheet">Excel读取的一个表单</param>
+        /// <param name="headerRows">表头有几行</param>
+        public SQLExporter(DataTable sheet, int headerRows)
+        {
+            m_sheet = sheet;
+            m_headerRows = headerRows;
+			DataColumnCollection columns = sheet.Columns;
+			if (columns != null && columns.Count > 0)
+			{
+				_types = new RowType[columns.Count];
+				int firstDataRow = m_headerRows - 1;
+				for (int i = firstDataRow; i < sheet.Rows.Count; i++)
+				{
+					DataRow row = sheet.Rows[i];
+					for (int j = 0; j < columns.Count; j ++ )
+					{
+						if (_types[j] == RowType.text)
+						{
+							continue;
+						}
+
+						DataColumn column = columns[j];
+						object value = row[column];
+						Type type = value.GetType();
+						if (type == typeof(double))
+						{
+							double num = (double)value;
+							if ((int)num != num)
+							{
+								_types[j] = RowType.text;
+							}
+						}
+						else if (type == typeof(string))
+						{
+							int var = 0;
+							string str = value.ToString();
+							if (string.IsNullOrEmpty(str))
+							{
+								continue;
+							}
+							else if (int.TryParse((string)value, out var) == false || var.ToString() != (string)value)
+							{
+								_types[j] = RowType.text;
+							}
+						}
+					}
+				}
+			}
+        }
+
+        /// <summary>
+        /// 转换成SQL字符串，并保存到指定的文件
+        /// </summary>
+        /// <param name="filePath">存盘文件</param>
+        /// <param name="encoding">编码格式</param>
+		public void SaveToFile(Options options, Encoding encoding, string tableName)
+        {
+            //-- 转换成SQL语句
+			string mysql_header = null;
+			string sqlite_header = null;
+            GetTabelStructSQL(m_sheet, options.DbName,tableName, out sqlite_header, out mysql_header);
+            string tabelContent = GetTableContentSQL(m_sheet, tableName);
+
+            if(!Directory.Exists(options.WorkOut))
+                Directory.CreateDirectory(options.WorkOut);
+
+			bool[] build = { options.sqlite, options.mysql};
+			string[] headers = { sqlite_header, mysql_header};
+            string[] filePaths = { options.WorkOut + "\\" + options.SQLPath + "_sqlite.sql", options.WorkOut + "\\" + options.SQLPath + ".sql" };
+
+			for (int i = 0; i < 2; i ++ )
+			{
+				//-- 保存文件
+				if(build[i])
+				{
+					string filePath = filePaths[i];
+                    if (File.Exists(filePath))
+                        File.Delete(filePath);
+
+					using (FileStream file = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+					{
+						using (TextWriter writer = new StreamWriter(file, encoding))
+						{
+							writer.Write(headers[i]);
+							writer.WriteLine();
+							writer.Write(tabelContent);
+							writer.WriteLine();
+						}
+					}
+				}
+			}
+        }
+
+        /// <summary>
+        /// 将表单内容转换成INSERT语句
+        /// </summary>
+        private string GetTableContentSQL(DataTable sheet, string tabelName)
+        {
+            StringBuilder sbContent = new StringBuilder();
+            StringBuilder sbNames = new StringBuilder();
+            StringBuilder sbValues = new StringBuilder();
+
+            //-- 字段名称列表
+            foreach (DataColumn column in sheet.Columns)
+            {
+                if (column.ToString().Length>1)
+                { 
+                    sbNames.Append(column.ToString());
+                    sbNames.Append(", ");
+                }
+            }
+
+            //-- 逐行转换数据
+            int firstDataRow = m_headerRows - 1;
+            for (int i = firstDataRow; i < sheet.Rows.Count; i++ )
+            {
+                DataRow row = sheet.Rows[i];
+				sbValues.Remove(0, sbValues.Length);
+
+				int j = 0;
+				DataColumnCollection columns = sheet.Columns;
+				for (j = 0; j < columns.Count; j ++ )
+				{
+					DataColumn column = columns[j];
+                    object value = row[column];
+
+                    if (String.IsNullOrEmpty(value.ToString()) && value.ToString().Length>1 && (j == 0))//处理第一列填并且若为空 直接跳过 后面列值为空或不填的情况
+                    {
+                        continue;
+                    }
+                    
+					if (sbValues.Length > 0)
+					{
+						sbValues.Append(", ");
+					}
+					
+					Type type = value.GetType();
+					if (type == typeof(System.DBNull))
+					{
+						if(j == 0)
+						{
+							break;
+						}
+                        sbValues.Append(@"'NULL'");
+					}
+					else
+					{
+						sbValues.AppendFormat("'{0}'", value.ToString());
+					}
+				}
+
+				if(j > 0)
+				{
+                    if (sbValues.Length > 0)
+                    {
+                        sbValues.Append(",'NULL','NULL'");
+                        sbContent.AppendFormat("INSERT INTO `{0}` VALUES({1});\n", tabelName, sbValues.ToString());
+                    }
+				}
+            }
+            sbContent = sbContent.Replace(@"\",@"\\");
+            return sbContent.ToString();
+        }
+
+        /// <summary>
+        /// 根据表头构造CREATE TABLE语句
+        /// </summary>
+        private void GetTabelStructSQL(DataTable sheet, string dbName, string tabelName, out string sqlite_header, out string mysql_header)
+        {
+			sqlite_header = "";
+			mysql_header = "";
+
+            // sqlite 删除表并重新创建
+            StringBuilder sqlite = new StringBuilder();
+            sqlite.AppendFormat("DROP TABLE IF EXISTS `{0}`;\n", dbName);
+            sqlite.AppendFormat("CREATE TABLE `{0}` (\n", dbName);
+
+            //mysql 创建库并使用状态
+			StringBuilder mysql = new StringBuilder();
+            mysql.AppendFormat("CREATE DATABASE IF NOT EXISTS `{0}`;\n", dbName);
+            mysql.AppendFormat("\r\n");
+            mysql.AppendFormat("USE `{0}`;\n", dbName);
+            mysql.AppendFormat("\r\n");
+            mysql.AppendFormat("DROP TABLE IF EXISTS `{0}`;\n", tabelName);
+            mysql.AppendFormat("\r\n");  
+			mysql.AppendFormat("CREATE TABLE IF NOT EXISTS `{0}` (\n", tabelName);
+
+            //遍历列名
+			DataColumn column = null;
+			DataColumnCollection columns = sheet.Columns;
+			for (int i = 0; i < columns.Count; i ++ )
+			{
+				column = columns[i];
+				string filedName = column.ToString();
+
+                if (filedName.Contains("Column")!=true)
+                {
+                    sqlite.AppendFormat("`{0}` TEXT,\n", filedName);
+                    mysql.AppendFormat("`{0}` TEXT,\n", filedName);
+                }
+			}
+            //补充Tag Result 列
+            mysql.AppendFormat("`{0}` TEXT,\n", "Tag");
+            mysql.AppendFormat("`{0}` TEXT \n", "Result");
+            mysql.AppendFormat("\r\n");
+
+            sqlite.AppendLine("\n);");
+			mysql.AppendFormat("\n)  ENGINE=InnoDB DEFAULT CHARSET=utf8;\r\n");
+
+			sqlite_header = sqlite.ToString();
+			mysql_header = mysql.ToString();
+        }
+    }
+}
---
+++ ExcelToMySql/Program.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Data;
+using excel2json;
+
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Collections.Generic;
+
+namespace ExcelToMySql
+{
+    class Program
+    {
+
+        static void Export(Options options)
+        {
+            string excelPath = options.ExcelPath;
+            int header = options.HeaderRows;
+            string fileName = Path.GetFileNameWithoutExtension(excelPath);
+            String strMsg = "";
+            DataSet dt = ExcelToDataSet("遍历数据.xlsx", out strMsg);
+            DataSet book = dt;
+
+
+            // 数据检测
+            if (book.Tables.Count < 1)
+            {
+                throw new Exception("Excel文件中没有找到Sheet");
+            }
+
+            // 取得数据
+            for (int mark = 0; mark < book.Tables.Count; mark++)
+            {
+                DataTable sheet = book.Tables[mark];
+                if (sheet.Rows.Count <= 0)
+                {
+                    continue;
+                    //throw new Exception("Excel Sheet中没有数据");
+                }
+
+                //-- 确定编码
+                Encoding cd = new UTF8Encoding(false);
+                if (options.Encoding != "utf8-nobom")
+                {
+                    foreach (EncodingInfo ei in Encoding.GetEncodings())
+                    {
+                        Encoding e = ei.GetEncoding();
+                        if (e.EncodingName == options.Encoding)
+                        {
+                            cd = e;
+                            break;
+                        }
+                    }
+                }
+
+                //-- 导出SQL文件
+                if (options.sqlite || options.mysql)
+                {
+                    SQLExporter exporter = new SQLExporter(sheet, header);
+                    if (string.IsNullOrEmpty(options.SQLPath))
+                    {
+                        options.SQLPath = string.Format("{0}/{1}", options.WorkOut, fileName);
+                    }
+                    options.SQLPath = sheet.TableName.ToString();
+                    exporter.SaveToFile(options, cd, sheet.TableName.ToString());
+                }
+            }
+            //}
+        }
+
+        /// <summary>
+        /// Excel转换成DataTable（.xls）
+        /// </summary>
+        /// <param name="filePath">Excel文件路径</param>
+        /// <returns></returns>
+        public static DataTable ExcelToDataTable(string filePath)
+        {
+            var dt = new DataTable();
+            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var hssfworkbook = new HSSFWorkbook(file);
+                var sheet = hssfworkbook.GetSheetAt(0);
+                for (var j = 0; j < 5; j++)
+                {
+                    dt.Columns.Add(Convert.ToChar(((int)'A') + j).ToString());
+                }
+                var rows = sheet.GetRowEnumerator();
+                while (rows.MoveNext())
+                {
+                    var row = (HSSFRow)rows.Current;
+                    var dr = dt.NewRow();
+                    for (var i = 0; i < row.LastCellNum; i++)
+                    {
+                        var cell = row.GetCell(i);
+                        if (cell == null)
+                        {
+                            dr[i] = null;
+                        }
+                        else
+                        {
+                            switch (cell.CellType)
+                            {
+                                case CellType.Blank:
+                                    dr[i] = "[null]";
+                                    break;
+                                case CellType.Boolean:
+                                    dr[i] = cell.BooleanCellValue;
+                                    break;
+                                case CellType.Numeric:
+                                    dr[i] = cell.ToString();
+                                    break;
+                                case CellType.String:
+                                    dr[i] = cell.StringCellValue;
+                                    break;
+                                case CellType.Error:
+                                    dr[i] = cell.ErrorCellValue;
+                                    break;
+                                case CellType.Formula:
+                                    try
+                                    {
+                                        dr[i] = cell.NumericCellValue;
+                                    }
+                                    catch
+                                    {
+                                        dr[i] = cell.StringCellValue;
+                                    }
+                                    break;
+                                default:
+                                    dr[i] = "=" + cell.CellFormula;
+                                    break;
+                            }
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// Excel转换成DataSet（.xlsx/.xls）
+        /// </summary>
+        /// <param name="filePath">Excel文件路径</param>
+        /// <param name="strMsg"></param>
+        /// <returns></returns>
+        public static DataSet ExcelToDataSet(string filePath, out string strMsg)
+        {
+            strMsg = "";
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            string fileType = Path.GetExtension(filePath).ToLower();
+            string fileName = Path.GetFileName(filePath).ToLower();
+            try
+            {
+                ISheet sheet = null;
+                int sheetNumber = 0;
+                // 加载Excel文件
+                using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    if (fileType == ".xlsx")
+                    {
+                        // 2007版本
+                        XSSFWorkbook workbook = new XSSFWorkbook(fs);
+                        sheetNumber = workbook.NumberOfSheets;
+                        for (int i = 0; i < sheetNumber; i++)
+                        {
+                            string sheetName = workbook.GetSheetName(i);
+                            sheet = workbook.GetSheet(sheetName);
+                            if (sheet != null)
+                            {
+                                dt = GetSheetDataTable(sheet, out strMsg);
+                                if (dt != null)
+                                {
+                                    dt.TableName = sheetName.Trim();
+                                    ds.Tables.Add(dt);
+                                }
+                                else
+                                {
+                                    //MessageBox.Show("Sheet数据获取失败，原因：" + strMsg);
+                                }
+                            }
+                        }
+                    }
+                    else if (fileType == ".xls")
+                    {
+                        // 2003版本
+                        HSSFWorkbook workbook = new HSSFWorkbook(fs);
+                        sheetNumber = workbook.NumberOfSheets;
+                        for (int i = 0; i < sheetNumber; i++)
+                        {
+                            string sheetName = workbook.GetSheetName(i);
+                            sheet = workbook.GetSheet(sheetName);
+                            if (sheet != null)
+                            {
+                                dt = GetSheetDataTable(sheet, out strMsg);
+                                if (dt != null)
+                                {
+                                    dt.TableName = sheetName.Trim();
+                                    ds.Tables.Add(dt);
+                                }
+                                else
+                                {
+                                    //MessageBox.Show("Sheet数据获取失败，原因：" + strMsg);
+                                }
+                            }
+                        }
+                    }
+                }
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                strMsg = ex.Message;
+                return null;
+            }
+
+
+        }
+        /// <summary>
+        /// 获取sheet表对应的DataTable
+        /// </summary>
+        /// <param name="sheet">Excel工作表</param>
+        /// <param name="strMsg"></param>
+        /// <returns></returns>
+        private static DataTable GetSheetDataTable(ISheet sheet, out string strMsg)
+        {
+            strMsg = "";
+            DataTable dt = new DataTable();
+            string sheetName = sheet.SheetName;
+            int startIndex = 0;// sheet.FirstRowNum;
+            int lastIndex = sheet.PhysicalNumberOfRows;
+            //最大列数
+            int cellCount = 0;
+            IRow maxRow = sheet.GetRow(0);
+            for (int i = startIndex; i <= lastIndex; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row != null && cellCount < row.LastCellNum)
+                {
+                    cellCount = row.LastCellNum;
+                    maxRow = row;
+                }
+            }
+
+            //列名设置
+            try
+            {
+                for (int i = 0; i < maxRow.LastCellNum; i++)//maxRow.FirstCellNum
+                {
+                    String temp = maxRow.GetCell(i).StringCellValue;
+                    dt.Columns.Add(temp);
+                }
+            }
+            catch
+            {
+                strMsg = "工作表" + sheetName + "中无数据";
+                return null;
+            }
+
+            //数据填充
+            for (int i = 1; i <= lastIndex; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                DataRow drNew = dt.NewRow();
+                if (row != null)
+                {
+                    for (int j = row.FirstCellNum; j < row.LastCellNum; ++j)
+                    {
+                        if (row.GetCell(j) != null)
+                        {
+                            ICell cell = row.GetCell(j);
+                            switch (cell.CellType)
+                            {
+                                case CellType.Blank:
+                                    drNew[j] = "";
+                                    break;
+                                case CellType.Numeric:
+                                    short format = cell.CellStyle.DataFormat;
+                                    //对时间格式（2015.12.5、2015/12/5、2015-12-5等）的处理
+                                    if (format == 14 || format == 31 || format == 57 || format == 58)
+                                        drNew[j] = cell.DateCellValue;
+                                    else
+                                        drNew[j] = cell.NumericCellValue;
+                                    if (cell.CellStyle.DataFormat == 177 || cell.CellStyle.DataFormat == 178 || cell.CellStyle.DataFormat == 188)
+                                        drNew[j] = cell.NumericCellValue.ToString("#0.00");
+                                    break;
+                                case CellType.String:
+                                    drNew[j] = cell.StringCellValue;
+                                    break;
+                                case CellType.Formula:
+                                    try
+                                    {
+                                        drNew[j] = cell.NumericCellValue;
+                                        if (cell.CellStyle.DataFormat == 177 || cell.CellStyle.DataFormat == 178 || cell.CellStyle.DataFormat == 188)
+                                            drNew[j] = cell.NumericCellValue.ToString("#0.00");
+                                    }
+                                    catch
+                                    {
+                                        try
+                                        {
+                                            drNew[j] = cell.StringCellValue;
+                                        }
+                                        catch { }
+                                    }
+                                    break;
+                                default:
+                                    drNew[j] = cell.StringCellValue;
+                                    break;
+                            }
+                        }
+                    }
+                }
+                dt.Rows.Add(drNew);
+            }
+            return dt;
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("传参个数不正确,请检测数据传参！");
+                return;
+            }
+
+            String busType = args[0].ToString();
+            if (busType.Length <= 0)
+            {
+                Console.WriteLine("传参值正确,请检测数据传参！");
+                return;
+            }
+
+
+            Options option = new Options();
+            option.HeaderRows = 1;
+            option.WorkOut = "ExportMySql";
+            option.mysql = true;
+            option.SQLPath = "test_type";
+            String filePath = @"遍历数据.xlsx";
+            option.ExcelPath = filePath;
+
+            if (busType.Contains("NeManager"))
+                option.DbName = "unm2000nemanager";
+
+            if (busType.Contains("TP"))
+                option.DbName = "unm2000autotp";
+
+            if (busType.Contains("VC"))
+                option.DbName = "unm2000autovc";
+
+            if (busType.Contains("OTN"))
+                option.DbName = "unm2000autootn";
+
+            if (busType.Contains("PTN"))
+                option.DbName = "unm2000autoptn";
+
+            if (Directory.Exists(option.WorkOut) == false)
+                Directory.CreateDirectory(option.WorkOut);
+
+            if (File.Exists(filePath))
+                Export(option);
+            else
+                Console.WriteLine("Excel文件不存在！");
+        }
+    }
+}
